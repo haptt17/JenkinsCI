74eca94fe969b177d21a933b637fe30d80a3e960
Pham Thi Thu Ha <133737630+haptt17@users.noreply.github.com>
2024-04-02 11:59:19 +0700

    Update sendEmail.groovy

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -1,4 +1,6 @@
+#! /usr/bin/env groovy
 import hudson.plugins.emailext.ExtendedEmailPublisher
 def call() {
+    email='$recipientEmail'
     emailext body: 'Build Thành Công1', compressLog: true, subject: 'Test', to: ${email}
 }
