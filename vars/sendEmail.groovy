0ba88f49a903764dbb59db350e341d9d4cedf8fa
Pham Thi Thu Ha <133737630+haptt17@users.noreply.github.com>
2024-04-02 16:58:18 +0700

    Update sendEmail.groovy

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -3,5 +3,7 @@ import hudson.plugins.emailext.ExtendedEmailPublisher
 
 def call(String body, String subject, String recipientEmail,) {
     def email = params.recipientEmail
+    body = body // User input for body
+    subject = subject // User input for subject
     emailext body: body, compressLog: true, subject: subject, to: email
 }
