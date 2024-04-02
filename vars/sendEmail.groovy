4a9e97bf6bdd0151e5f3d18032e2541bd0bc7947
Pham Thi Thu Ha <133737630+haptt17@users.noreply.github.com>
2024-04-02 11:25:46 +0700

    Update sendEmail.groovy

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -1,8 +1,8 @@
 import hudson.plugins.emailext.ExtendedEmailPublisher
 
-def call(String subject, String mimeType = 'text/plain', boolean attachLog = false) {
+def sendEmail(String subject, String mimeType = 'text/plain', boolean attachLog = false) {
     emailext subject: subject,
-             to: params.recipientEmail,
+             to: ${params.recipientEmail},
              mimeType: mimeType,
              attachLog: attachLog
 }
